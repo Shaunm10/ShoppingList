22b56de7a1ba7b87d101773dd3e61c94247d528f
shaunsargent <ShaunM10@gmail.com>
2024-01-01 17:44:35 -0500

    WIP

--- app/src/main/java/com/example/shoppinglist/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingList.kt
@@ -1,6 +1,7 @@
 package com.example.shoppinglist
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,9 +13,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,6 +31,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
@@ -63,9 +70,15 @@ fun ShoppingListApp() {
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-            items(items) {
-                // create a new ShoppingListItme for each of the items in our list (state)
-                ShoppingListItem(it, { }, { })
+            items(items) { shoppingItem ->
+                // create a new ShoppingListItem for each of the items in our list (state)
+                ShoppingListItem(
+                    shoppingItem,
+                    {},
+                    {
+                        items = items.filter { it.id != shoppingItem.id }
+                    }
+                )
             }
         }
     }
@@ -138,6 +151,26 @@ fun ShoppingListApp() {
 }
 
 @Composable
+fun ShoppingItemEditor(
+    item: ShoppingItem,
+    onEditComplete: (String, Int) -> Unit
+) {
+    var editingName by remember { mutableStateOf(item.name) }
+    var editingQuantity by remember { mutableStateOf(item.quantity.toString()) }
+    var isEditing by remember { mutableStateOf(item.isEditing) }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Gray)
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+
+    }
+
+}
+
+@Composable
 fun ShoppingListItem(
     item: ShoppingItem,
     onEditClick: () -> Unit,
@@ -150,21 +183,26 @@ fun ShoppingListItem(
             .border(
                 border = BorderStroke(2.dp, Color(0XFF018786)),
                 shape = RoundedCornerShape(20)
-            )
+            ),
+        horizontalArrangement = Arrangement.SpaceBetween
 
     ) {
         Text(
             text = item.name,
             modifier = Modifier.padding(8.dp)
         )
-        Text(text = "Qty: ${item.quantity}")
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Edit")
-
-        }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Delete")
-
-        }
-    }
+        Text(
+            text = "Qty: ${item.quantity}",
+            modifier = Modifier.padding(8.dp)
+        )
+        Row(modifier = Modifier.padding(8.dp)) {
+            IconButton(onClick = onEditClick)
+            {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit")
+            }
+            IconButton(onClick = onDeleteClick) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
+            }
+        }// end Row
+    }// end Row
 }
