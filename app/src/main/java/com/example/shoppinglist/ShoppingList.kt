da24363ff55ab8ff060642e47ad4be70eb49ba6f
shaunsargent <ShaunM10@gmail.com>
2023-12-31 23:42:31 -0500

    Made the quanity texbox only accept numbers

--- app/src/main/java/com/example/shoppinglist/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingList.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
 class ShoppingList {
@@ -58,10 +60,8 @@ fun ShoppingListApp() {
                 .padding(16.dp)
         ) {
             items(items) {
-
             }
         }
-
     }
 
     if (showDialog) {
@@ -84,10 +84,13 @@ fun ShoppingListApp() {
                                 itemQuantity.toIntOrNull() ?: 1,
                                 false
                             )
-                            
+
                             // this adds the item to the list.
                             items = items + shoppingItem
 
+                            // reset the state
+                            itemName = ""
+                            itemQuantity = "1"
                             showDialog = false
                         }
 
@@ -113,6 +116,7 @@ fun ShoppingListApp() {
                     )
                     OutlinedTextField(
                         value = itemQuantity,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                         onValueChange = {
                             itemQuantity = it
                         },
